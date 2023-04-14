869d0affd4c8f8e999d78a1ff8f219f15988e6de
tmontanaro <teodoro.montanaro@unisalento.it>
2023-04-14 16:47:55 +0200

    final version

--- sendDataToServer.js
+++ sendDataToServer.js
@@ -1,5 +1,6 @@
 
 const sensorLib = require('node-dht-sensor'); // include existing module called ‘node-dht-sensor’
+const http = require('node:http');
 
 // Setup sensor, exit if failed
 var sensorType = 11; // 11 for DHT11, 22 for DHT22 and AM2302
@@ -17,6 +18,45 @@ setInterval(function() {
   var readout = sensorLib.read();
   
   console.log('Temperature:', readout.temperature.toFixed(1) + 'C');
-  console.log('Humidity: ', readout.humidity.toFixed(1) + '%');
+  //console.log('Humidity: ', readout.humidity.toFixed(1) + '%');
+
+
+  const postData = JSON.stringify({
+    'sensor': 'ID1',
+    'timestamp': 12345678,
+    'temperature': readout.temperature.toFixed(1)
+  });
+
+  const options = {
+    hostname: '192.168.1.250',
+    port: 3000,
+    path: '/temperature',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(postData),
+    },
+  };
+
+  const req = http.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+    res.on('end', () => {
+      console.log('No more data in response.');
+    });
+  });
+
+  req.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+
+// Write data to request body
+  req.write(postData);
+  req.end();
+
 }, 2000);
 
